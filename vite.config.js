80ab2eb7f8d2edf0a2bd7aa97c6404794e10ec11
trokute <ravenmydino@gmail.com>
2026-09-23 03:40:23 +0000

    guh!

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
+	base: '/5bLOCKS/',
 	plugins: [svelte()],
 	server: { host: true }
 })
