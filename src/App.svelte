2e2a0e74ddc79eda47cfdef6ab6651450a4ea208
trokute <ravenmydino@gmail.com>
2026-09-23 03:39:13 +0000

    patch

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,24 @@
 <script>
 import { onMount } from 'svelte'
+import { POOL } from './blocks.js'
 
-const POOL={'102':'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30"><path fill="#FFAD04" stroke="none" d="M 0 0L 0 30 30 30 30 0 Z"/></svg>','21':'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30"><path fill="#000000" stroke="none" d="M 0 18.75L 0 26.25 30 22.5 0 18.75M 0 26.25L 0 30 30 30 0 26.25M 0 0L 0 3.75 30 0 0 0M 0 3.75L 0 11.25 30 7.5 0 3.75M 0 11.25L 0 18.75 30 15 0 11.25 Z"/></svg>','89':'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30"><path fill="#707070" stroke="none" d="M 20 6.05L 17.5 6.05 17.5 0 12.5 0 12.5 6.05 10 6.05 10 11.05 12.5 11.05 12.5 21.05 10 21.05 10 26.05 12.5 26.05 12.5 30 17.5 30 17.5 26.05 20 26.05 20 21.05 17.5 21.05 17.5 11.05 20 11.05 20 6.05 Z"/></svg>','42':'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30"><path fill="#303030" stroke="none" d="M 0 0L 0 30 30 30 30 0 Z"/><path fill="#202020" stroke="none" d="M 30 7.3L 30 0 0 0 0 7.4 3.6 10 8.2 7.5 14.15 13 20.8 4.55 25.5 9.45 30 7.3 Z"/><path fill="#101010" stroke="none" d="M 0 0L 30 0 30 2.4 0 2.4 Z"/></svg>','134':'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" x="0px" y="0px" width="32px" height="31px" viewBox="-1 -1 32 31"><path stroke="#0066FF" stroke-width="1" stroke-linejoin="round" stroke-linecap="round" fill="none" d="M 2 0L 0 0 0 1M 9 0L 6 0M 16 0L 13 0M 23 0L 20 0M 30 3L 30 0 27 0M 30 10L 30 10M 23 10L 26 10M 16 10L 19 10M 9 10L 12 10M 2 10L 5 10M 0 5L 0 8"/></svg>','34':'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" x="0px" y="0px" width="32px" height="32px" viewBox="-1 -1 32 32"><path stroke="#D5AA00" stroke-width="1" stroke-linejoin="round" stroke-linecap="round" fill="none" d="M 29.6 18.05L 29.6 15.05M 29.6 11.05L 29.6 8.05M 29.55 4.15Q 29.3 2.45 28.3 1.55M 24.5 0.4L 21.5 0.4M 0.4 13.1L 0.4 16.1M 17.5 0.4L 14.5 0.4M 10.5 0.4L 7.5 0.4M 3.55 0.6Q 1.95 1 1.15 2.2M 0.4 6.1L 0.4 9.1M 0.4 20.1L 0.4 23.1M 0.8 27.05Q 1.35 28.55 2.85 29.15M 6.8 29.6L 9.8 29.6M 13.8 29.6L 16.8 29.6M 20.8 29.6L 23.8 29.6M 27.7 28.95Q 29 28.15 29.4 26.5M 29.6 25.05L 29.6 22.05"/></svg>','55':'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30"><path fill="#303030" stroke="none" d="M 30 30L 30 0 0 0 0 30 30 30 Z"/><path fill="#181818" stroke="none" d="M 1.4 27.95L 5 28.95 2.4 25.4 1.4 27.95 Z"/><path fill="#655B3A" stroke="none" d="M 15.1 24L 19.5 19.15 15.1 20.75 15.1 24 Z"/><path fill="#505050" stroke="none" d="M 12.65 3.45L 10.5 7.9 5.9 6.35 3.8 9.6 7.75 10.05 8.6 12.85 14.85 8.15 12.65 3.45 Z"/><path fill="#61413F" stroke="none" d="M 23.75 2.6L 22.4 5.4 25.05 8.05 20.85 9.35 28 9.7 26.1 5.75 26.9 4.4 23.75 2.6 Z"/><path fill="#FFFFFF" fill-opacity="0.4980392156862745" stroke="none" d="M 30 -0.05L 0 -0.05 0 9.95 30 9.95 30 -0.05 Z"/><path fill="#FFFFFF" stroke="none" d="M 30 3L 30 0 0 0 0 3 30 3 Z"/></svg>','78':'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" x="0px" y="0px" width="32px" height="32px" viewBox="-1 -1 32 32"><path id="Layer0_0_1_STROKES" stroke="#FFCC00" stroke-width="1" stroke-linejoin="round" stroke-linecap="round" fill="none" d="M 24.65 3.15L 22.05 4.75M 29.25 0.9L 29.75 0 28.05 1.05M 25.65 6.9L 27.15 4.3M 23.25 12.4L 22.45 12.1 23.55 10.3M 29.75 15L 26.95 13.9M 25.65 23.25L 24.15 20.65M 23.05 17.65L 25.85 16.55M 18.65 6.85L 17.75 7.35 17.05 5.55M 15.6 1.8L 14.9 0.1 14.5 1.2M 13 4.9L 12 7.35 11.7 7.15M 8.3 5.05L 5.7 3.5M 2.3 1.4L 0 0M 2.25 3.7L 3.75 6.3M 5.85 9.7L 7.25 12.1M 5.9 20.05L 4.4 22.65M 3.15 16.25L 5.95 17.35M 3.35 13.7L 0.6 14.8M 15.5 28.35L 16.6 25.55M 12.9 24.85L 14 27.65M 2.2 28.6L 4.8 27M 2.3 26.05L 0.8 28.6M 29.35 29.3L 27.75 26.7M 24.45 26.65L 27.05 28.2M 18.45 22.95L 21.05 24.55M 8.2 24.9L 10.8 23.3"/></svg>','0':'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30"><defs><g id="Layer1_0_FILL"><path fill="#00FFFF" fill-opacity="0.5019607843137255" stroke="none" d="M 0 0L 0 30 30 30 30 0 0 0 Z"/></g><g id="Layer0_0_FILL"><path fill="#000000" stroke="none" d="M 26.7 12.2Q 26.7 11.25 26.2 10.55 25.75 9.8 24.95 9.5 24.2 9.2 22.45 9.2L 17.9 9.2 17.9 19.95 20.05 19.95 20.05 15.45 20.5 15.45Q 21.25 15.45 21.6 15.6 21.95 15.7 22.25 16.05 22.55 16.35 23.4 17.6L 24.95 19.95 27.55 19.95 26.25 17.85Q 25.45 16.6 25 16.1 24.55 15.6 23.85 15.2 25.25 15 25.95 14.2 26.7 13.45 26.7 12.2M 23.35 11.05Q 23.85 11.15 24.15 11.5 24.45 11.8 24.45 12.35 24.45 12.85 24.25 13.15 24 13.5 23.6 13.6 23.25 13.75 21.65 13.75L 20.05 13.75 20.05 11.05 23.35 11.05M 15.85 9.2L 13.7 9.2 13.7 19.95 15.85 19.95 15.85 9.2M 8.3 9.2L 6.05 9.2 1.85 19.95 4.15 19.95 5.05 17.5 9.35 17.5 10.25 19.95 12.6 19.95 8.3 9.2M 8.65 15.7L 5.7 15.7 7.15 11.7 8.65 15.7 Z"/></g></defs><g transform="matrix( 1, 0, 0, 1, 0,0) "><use xlink:href="#Layer1_0_FILL"/></g><g transform="matrix( 1, 0, 0, 1, 0,0) "><use xlink:href="#Layer0_0_FILL"/></g></svg>','50':'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="none" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30"><path fill="#CCCCCC" fill-opacity="0.37" stroke="none" d="M 5.35 13.65L 2.5 18.55 4.95 30 9.9 30 5.35 13.65M 19 13.35L 13.6 15.15 12.4 24.5 14.1 30 15.05 30 19.05 13.45 19.1 13.3 19 13.35 Z"/><path fill="#999999" fill-opacity="0.37" stroke="none" d="M 9.7 15.85L 5.35 13.65 9.9 30 14.1 30 12.4 24.5 9.7 15.85M 24.5 21.05L 23.05 21.05 18.85 26.15Q 20.05 21.75 21.1 15.85L 19.05 13.45 15.05 30 18.55 30 24.5 21.05 Z"/><path fill="#666666" fill-opacity="0.37" stroke="none" d="M 27.6 24L 25 21.05 24.5 21.05 18.55 30 22.3 30 27.6 24 Z"/><path fill="#333333" stroke="none" d="M 26.25 30.1Q 27 30.1 27.55 29.55 28.1 29.05 28.15 28.4 28.1 27.7 27.55 27.15 27 26.65 26.25 26.65 25.45 26.65 24.9 27.15 24.35 27.7 24.35 28.4L 24.4 28.8 24.15 28.75Q 23.85 28.7 23.6 28.95L 23.4 29.65 23.6 30.25 24.15 30.55 24.6 30.25 24.9 29.65 24.9 29.55Q 25.45 30.1 26.25 30.1M 4.25 29.85L 4.6 29.1 4.25 28.2Q 3.95 27.9 3.45 27.9 2.95 27.9 2.6 28.2 2.3 28.6 2.3 29.1 2.3 29.55 2.6 29.85 2.95 30.25 3.45 30.25 3.95 30.25 4.25 29.85M 16.7 30.05Q 17.1 29.6 16.95 28.95 16.9 28.3 16.35 27.75 15.8 27.2 15.05 27.05 14.5 27 14.1 27.4 13.65 27.8 13.7 28.45 13.85 29.1 14.45 29.7 14.95 30.2 15.55 30.3 16.3 30.45 16.7 30.05 Z"/></svg>'}
 const IDS=Object.keys(POOL)
 const IMG={}
-for(const k of IDS){const im=new Image();im.src='data:image/svg+xml,'+encodeURIComponent(POOL[k]);IMG[k]=im}
+const IMGA={}
+for(const k of IDS){
+	const v=POOL[k]
+	if(Array.isArray(v)){
+		IMGA[k]=v.map(s=>{const im=new Image();im.src='data:image/svg+xml,'+encodeURIComponent(s);return im})
+	}else{
+		const im=new Image();im.src='data:image/svg+xml,'+encodeURIComponent(v);IMG[k]=im
+	}
+}
+let animT=0
+function curImg(id){
+	const arr=IMGA[id]
+	if(arr)return arr[Math.floor(animT/6)%arr.length]
+	return IMG[id]
+}
 const FONT="'Shag Lounge', Helvetica, sans-serif"
 
 let cv,cx
@@ -108,7 +122,7 @@ function dTile(id,x,y,s,tint){
 	cx.strokeStyle='#2b2b2b'
 	cx.strokeRect(x,y,s,s)
 	if(id!==null){
-		const im=IMG[id]
+		const im=curImg(id)
 		const pad=s*0.16
 		if(im.complete)cx.drawImage(im,x+pad,y+pad,s-pad*2,s-pad*2)
 	}
@@ -197,7 +211,7 @@ function dFill(){
 	}else fillWait++
 	for(let i=0;i<fillN;i++){
 		const g=fillGrid[i]
-		const im=IMG[g.id]
+		const im=curImg(g.id)
 		if(im.complete)cx.drawImage(im,g.x,g.y,g.s,g.s)
 	}
 	if(fillWait>36){
@@ -256,6 +270,7 @@ function dShare(){
 	}
 }
 function frm(){
+	animT++
 	if(state==='play')dPlay()
 	else if(state==='fill')dFill()
 	else if(state==='share')dShare()
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-	base: '/5bLOCKS/',
 	plugins: [svelte()],
 	server: { host: true }
 })
